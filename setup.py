4e5d375f9c6c74c0b76d8a56bd4bc8911ffcfa1d
Gustavo Ximenez <134465101+ggximenez@users.noreply.github.com>
2024-10-11 18:40:40 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pnadium',  # Nome do pacote
-    version='0.1.0',  # Versão inicial
+    name='pnadium', 
+    version='0.1.0',  
     description='Pacote para download e processamento dos microdados da PNAD Contínua do IBGE.',
     long_description=open('README.md', encoding='utf-8').read(),  # Certifique-se de que o README.md existe
     long_description_content_type='text/markdown',
-    author='Seu Nome',  # Substitua pelo seu nome
-    author_email='ggximenez@gmail.com',  # Substitua pelo seu email
-    url='https://github.com/seu_usuario/pnadium',  # Substitua pelo URL do seu repositório, se houver
+    author='Gustavo G. Ximenez',
+    author_email='ggximenez@gmail.com',
+    url='https://github.com/ggximenez/pnadium',
     packages=find_packages(),
     install_requires=[
         'pandas',
@@ -18,7 +18,7 @@ setup(
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',  # Escolha a licença apropriada
+        'License :: MIT License', 
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
