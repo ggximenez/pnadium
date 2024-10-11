f016b23d956501f824311866ea93a22b9ef1884e
Gustavo Ximenez <134465101+ggximenez@users.noreply.github.com>
2024-10-11 19:32:13 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: MIT License', 
+        'License :: OSI Approved :: MIT License', 
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
